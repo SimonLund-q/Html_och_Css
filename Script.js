eebdc860b9dffaa9e780ce9c8734a29b4883364c
SimonLund-q <137650964+SimonLund-q@users.noreply.github.com>
2024-05-26 20:04:30 +0200

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,3 +1,13 @@
-function getColor(element){
+function getColor(){
+    element = document.getElementById("center");
+    button = document.getElementById("button");
+
     element.style.color="red";
+    element.style.width = "95%";
+    button.innerHTML = "fuck html, shits stupid";
+}
+function invertColors(){
+    element = document.getElementById("button");
+    color = element.style.color;
+    console.log(-color);
 }
